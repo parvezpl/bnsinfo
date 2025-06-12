b314942209cb6a97291ccd46a0eb02eab0b6d718
PARVEZ ALAM <136539319+parvezpl@users.noreply.github.com>
2025-06-12 11:03:07 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Head from 'next/head';
 import "./globals.css";
 import Navigation from "./utlty/navigation";
 
@@ -20,6 +21,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+<Head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7440927056234503"
+     crossorigin="anonymous"></script>
+</Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
