c89410931fadfa4f4a0c625bf1f758443816b5e4
parvezpl <parvezalam336@gmail.com>
2025-06-18 10:29:40 +0530

    filter

--- pages/api/bns/search.js
+++ pages/api/bns/search.js
@@ -9,17 +9,17 @@ export default async function handler(req, res) {
     if (req.method == "GET") {
 
         const { search, lang } = req.query
-        // console.log("Search query:", search, "Language:", lang);
+        const searchterm = search.split(" ")
+        console.log("Search query:", searchterm, "Language:", lang);
 
         try {
             if ("en" === "hi") {
                 const bns = await Bnshindis.find({
                     $text: { $search: search }
                 }).lean();
-
-                const data= bns.map(item => {
-                    return item.sections?.filter(res=>{
-                        if ( search.trim().toLowerCase() === res.section.trim().toLowerCase()) {
+                const data = bns.map(item => {
+                    return item.sections?.filter(res => {
+                        if (search.trim().toLowerCase() === res.section.trim().toLowerCase()) {
                             return res.section;
                         }
                     })[0];
@@ -30,33 +30,52 @@ export default async function handler(req, res) {
                     return res.status(404).json({ error: "No matching sections found" });
                 }
                 const filteredData = data.filter(item => item !== undefined && item !== null);
-                return res.status(200).json({bns:filteredData});
+                return res.status(200).json({ bns: filteredData });
 
 
             } else {
                 const bns = await Bnsenglishs.find({
                     $text: { $search: search }
                 }).lean();
-                console.log("Search results:", bns);
-                const data= bns.map(item => {
-                    return item.sections?.filter(res=>{
-                        if ( search.trim().toLowerCase() === res.section.trim().toLowerCase()) {
-                            return res.section;
+
+                const data = bns.map(item => {
+                    return item.sections?.filter(res => {
+                        let isMatch = false;
+                        searchterm.forEach(term => {
+                            if(term.trim().toLowerCase() === res.section.trim().toLowerCase()){
+                                isMatch = true;
+                            }
+                        })
+                        if (isMatch) {
+                            return true;
                         }
-                    })[0];
+                       
+                    });
+                })[0];
+                // console.log("Filtered sections:", data);
 
-                });
-                // console.log("Filtered sections:", search, data);
+                // const findbns= []
+                // const textdata = bns.map(item => {
+                //      const dd= item.sections?.map(res => {
+                //         findbns.push(res)
+                //         const filterdata=  searchterm.map(term => {
+                //             return findbns.filter(content=>content.section_title.trim().toLowerCase().includes(term.trim().toLowerCase()))
+                //         }) 
+                //         return filterdata[0]
+                //     })[0];
+                //     return dd
+                // })[0];
 
-                if (bns.length === 0) {
+                // console.log("Filtered sections:", textdata);
+
+                if (data.length === 0) {
                     return res.status(404).json({ error: "No matching sections found" });
                 }
 
 
-
-
                 const filteredData = data.filter(item => item !== undefined && item !== null);
-                return res.status(200).json({bns:filteredData});
+                // console.log("Filtered Data:", filteredData);
+                return res.status(200).json({ bns: filteredData });
             }
         } catch (error) {
             console.log(error)
